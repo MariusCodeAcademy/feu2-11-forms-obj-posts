d359af316612da4da5d3bce4716f9ebd8936ebad
Marius <marius@example.com>
2022-10-24 09:32:35 +0300

    form event listener

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 'use strict';
 console.log('main.js');
+
+const htmlEls = {
+  form: document.forms.createPost,
+  title: document.getElementById('title'),
+  image: document.getElementById('image'),
+  date: document.getElementById('date'),
+  author: document.getElementById('author'),
+  body: document.getElementById('body'),
+};
+
+// console.log('htmlEls ===', htmlEls);
+
+/**
+ * Main app function
+ */
+function init() {}
+
+// Event Listeners
+
+htmlEls.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Js is in control');
+
+  // sukurti formValues objekta
+  const formValues = {
+    title: '',
+  };
+});
